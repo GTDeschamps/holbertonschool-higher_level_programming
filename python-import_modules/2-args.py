c212112b09e49be557b6e2b8b3fb5ba3fc5237f1
GTDeschamps <g.deschamps@hotmail.fr>
2023-09-28 14:03:37 +0200

    corrected Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,15 +6,14 @@ def print_arg(argv):
 
     number_arg = len(argv)
     if number_arg == 0:
-        print("0 argument(s).")
-        print(".")
-    elif arg == 1:
-        print("{} argument :".format(number_arg))
+        print("0 arguments.")
+    elif number_arg == 1:
+        print("{} argument:".format(number_arg))
         print("1:", argv[0])
     else:
         print("{} arguments:".format(number_arg))
-        for i in range(number_arg):
-            print("{i+1}:".format(number_arg), argv[i])
+        for i in range( number_arg):
+            print("{}: {}".format(i+1, argv[i]))
 
 
 if __name__ == "__main__":
