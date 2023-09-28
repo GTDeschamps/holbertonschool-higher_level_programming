6ae24f01cdb5e572c711122040510c716521b897
GTDeschamps <g.deschamps@hotmail.fr>
2023-09-28 13:53:08 +0200

    Pycode Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 import sys
+
+
 def print_arg(argv):
+
     number_arg = len(argv)
-
-
     if number_arg == 0:
         print("0 argument(s).")
         print(".")
     elif arg == 1:
-        print ("{} argument :".format(number_arg))
+        print("{} argument :".format(number_arg))
         print("1:", argv[0])
     else:
         print("{} arguments:".format(number_arg))
         for i in range(number_arg):
-            print("{i+1}:".format(number_arg),argv[i])
+            print("{i+1}:".format(number_arg), argv[i])
+
 
 if __name__ == "__main__":
     arguments = sys.argv[1:]
