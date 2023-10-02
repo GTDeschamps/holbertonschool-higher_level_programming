e39c219a2b02525f81e70f596752ba9952fdf2fc
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-02 15:30:12 +0200

    corrected Task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for num in range(len(matrix[row])):
-            print('{:d}'.format(matrix[row][num]), end=' ')
-        print("$")
+            print('{:d}'.format(matrix[row][num]), end='')
+            if num != len(matrix)-1:
+                print(' ', end='')
+        print()
