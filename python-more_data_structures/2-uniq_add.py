aad4a4c269fe7856d48ca3b9afff4014b1a865a3
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-04 10:24:49 +0200

    Task 2 : add unique

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique = set()
+    total = 0
+
+    for num in my_list:
+        if num not in unique:
+            total += num
+            unique.add(num)
+
+    return total
