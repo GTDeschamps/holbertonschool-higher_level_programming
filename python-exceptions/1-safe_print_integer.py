39b9d6355d77e1f7db58b88e66599961a68ad43a
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-06 10:13:17 +0200

    task 1 corrected

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(int(value)))
-        return True
+        value_as_int = int(value)
+        if isinstance(value_as_int, int):
+            print("{:d}".format(int(value)))
+            return True
     except (ValueError, TypeError):
         return False
