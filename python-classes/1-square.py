ff32a0c45f1265809bb707ad71e919837ec51c04
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-09 12:45:20 +0200

    Task 1: first attribute: size

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Contains a class definition of 'Square'"""
+
+
+class Square:
+    """Definition of size in square"""
+    def __init__(self, size):
+        self.__size = size
