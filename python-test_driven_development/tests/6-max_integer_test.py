192005b885912a8c73365c1ffffe414db1c3e709
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-11 16:47:11 +0200

    corrected Task 5

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -2,27 +2,21 @@
 """Module for testing max integer condition in a list
 """
 import unittest
+max_integer = __import__("6-max_integer").max_integer
 
-class test_unit(unittest.TestCase):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
+class test_max_integer(unittest.TestCase):
+    """definition class test_max_integer for test"""
+
     def test_max_int(self):
-        numbers = [1, 2, 3, 4]
-        max_value = max(numbers)
-        self.assertEqual(max_value, 4)
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
 
-    def test_max_with_neg(self):
-        numbers = [-1, -2, -3, -4]
-        max_value = max(numbers)
-        self.assertEqual(max_value, -1)
+    def test_max__neg(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -1)
 
     def test_max_int_empty_list(self):
-        numbers = []
-        max_value = None
-        self.assertEqual(max_value, None)
+        self.assertEqual(max_integer(), None)
 
     def test_str_fail(self):
-        numbers = ["charlot", 66]
+        numbers = ["charlot", 4]
         with self.assertRaises(TypeError):
-            max_value = max(numbers)
+            max_value = max_integer(numbers)
