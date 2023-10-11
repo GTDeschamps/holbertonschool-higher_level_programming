fec0447ba390bbc4b1ac4ec517c4518c816649c8
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-11 12:34:10 +0200

    correction task 2: name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""definition of function say_my_name"""
+
 
 def say_my_name(first_name, last_name=""):
     """ function to print the quote "My name is <first name> <last name>"
