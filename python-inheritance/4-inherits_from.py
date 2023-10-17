746aa220c389fca4e134c92a77530ac71f79906f
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-17 14:38:57 +0200

    correction Task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,10 +3,7 @@
 
 
 def inherits_from(obj, a_class):
-    """function """
-    if isinstance(obj, a_class):
+    """function inherits """
+    if issubclass(type(obj), a_class):
         return True
-    for base_class in obj.__class__.__bases__:
-        if inherits_from(obj, base_class):
-            return True
     return False
