bd1504ae2718b76e4744869ec66ef6623dcc7b67
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-18 09:49:24 +0200

    Task 6: exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" definition of BaseGeometry class"""
+
+
+class BaseGeometry:
+    """ creation of class"""
+    def area(self):
+        """ implement geometry"""
+        raise Exception("area()is not implemented")
