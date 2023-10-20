e611260981aba76f4ddc2122c2485543b15913e4
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-20 11:23:51 +0200

    correction Task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,6 +4,7 @@
 
 def class_to_json(obj):
     """ function to return json dictionnary """
-    for key, val in obj.__dict__.items():
-        if hasattr(val, '__dict__'):
-            return {key: val}
+    if hasattr(obj, '__dict__'):
+        return obj.__dict__
+    else:
+        None
