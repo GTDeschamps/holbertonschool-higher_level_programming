1933ae0af554aeca05cfe73c7fd3323975d05a42
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-23 15:03:03 +0200

    corrrection Task 6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -54,7 +54,7 @@ class Rectangle(Base):
     def __str__(self):
         """ updating the class """
         return f"[Rectangle] \
-    ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
 
     @property
     def width(self):
