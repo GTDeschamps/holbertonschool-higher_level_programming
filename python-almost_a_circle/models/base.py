8a8bb74a22fcc3f3b24e393fc15f2ddfd3107ca5
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-23 11:10:12 +0200

    pycode of Task1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Fonction Base """
 
+
 class Base:
+    """ definition of id """
     __nb__objects = 0
 
     def __init__(self, id=None):
-        """ definition of id"""
         if id is not None:
             self.id = id
         else:
             self.__class__.__nb__objects += 1
             self.id = self.__class__.__nb__objects
-
