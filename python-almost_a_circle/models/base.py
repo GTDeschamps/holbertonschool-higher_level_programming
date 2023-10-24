6b08a4d4c0ad4805e759b8191b4791ce86c4efca
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-24 13:29:55 +0200

    correction Task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """Return the JSON string representation of list_dictionaries."""
-        if list_dictionaries is None or not list_dictionaries:
+        if list_dictionaries is None:
             return []
         else:
             return json.dumps(list_dictionaries)
