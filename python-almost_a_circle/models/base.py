9b522c0cf0f2bc126377a9efd3ac8d61ac1527ad
GTDeschamps <g.deschamps@hotmail.fr>
2023-10-24 14:53:41 +0200

    Task 16: Json string to file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,9 +16,23 @@ class Base:
             self.__class__.__nb__objects += 1
             self.id = self.__class__.__nb__objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Return the JSON string representation of list_dictionaries."""
         if list_dictionaries is None:
             return []
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file."""
+        if list_objs is None:
+            list_dicts = []
+
+        else:
+            list_dicts = cls.to_json_string(
+                [obj.to_dictionary() for obj in list_objs])
+        with open(cls.__name__ + ".json", 'w') as file:
+            file.write(list_dicts)
+
