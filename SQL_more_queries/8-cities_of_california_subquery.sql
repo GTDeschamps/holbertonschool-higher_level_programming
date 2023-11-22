b707c923712532c63147729a2ab39f81621d08dc
GTDeschamps <g.deschamps@hotmail.fr>
2023-11-22 10:29:47 +0100

    Task 8: list the cities

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- list all the cities of california --
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+  AND states.name = 'California'
+ORDER BY cities.id ASC;
