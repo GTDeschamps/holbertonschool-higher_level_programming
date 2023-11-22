6ad24267104669b996e12f0a3ddc284a1a483023
GTDeschamps <g.deschamps@hotmail.fr>
2023-11-22 12:10:36 +0100

    Task 11: Updated

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- list all the TV shows --
-SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, NULL) AS genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, IFNULL(tv_show_genres.genre_id, NULL) ASC;
