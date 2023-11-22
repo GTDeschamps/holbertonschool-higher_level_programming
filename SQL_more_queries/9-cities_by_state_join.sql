95d8ac47d1eda9d01ee6b9d059cc48143496858f
GTDeschamps <g.deschamps@hotmail.fr>
2023-11-22 10:50:30 +0100

    update Task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list all the cities by states --
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities, states
-WHERE cities.state_id = states.id
+SELECT cities.id, cities.name,
+(SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
+FROM cities
 ORDER BY cities.id ASC;
