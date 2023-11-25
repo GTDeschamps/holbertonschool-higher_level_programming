345acd97cbcd4a6d9e886bfd1623259d9af07cb2
GTDeschamps <g.deschamps@hotmail.fr>
2023-11-25 13:56:24 +0100

    Task 1: filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+"""List all States with a name begin by N """
+
+import MySQLdb
+import sys
+
+
+def list_states():
+    """List all states from databases hbtn_0e_0_usa"""
+
+    # Connect to the MySQL database
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Create a cursor object to interact with the database
+    cur = connection.cursor()
+
+    # Execute the SQL query
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Fetch all the rows from the query
+    query_rows = cur.fetchall()
+
+    # Display the results
+    for row in query_rows:
+        print(row)
+
+    # Close the cursor and connection
+    cur.close()
+    connection.close()
+
+
+if __name__ == "__main__":
+    """Execute the List_states function"""
+    list_states()
+
