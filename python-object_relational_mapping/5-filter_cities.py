4a6515d5d51888da1f00806217e0467a7f43deaa
GTDeschamps <g.deschamps@hotmail.fr>
2023-11-27 14:07:55 +0100

    Task 5: cities by State

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+"""List all cities by state from databases hbtn_0e_4_usa """
+
+
+import MySQLdb
+import sys
+
+
+def list_states():
+
+    # Connect to MySQL server
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+# Create a cursor object to interact with the database
+    cur = connection.cursor()
+
+    # Execute the query
+    cur.execute("SELECT * FROM cities WHERE state_name = %s ORDER BY id ASC")
+
+    # Fetch all the rows and display results
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    # Close connection
+    cur.close()
+    connection.close()
+
+
+if __name__ == "__main__":
+    """execute the list_states function"""
+    list_states()
