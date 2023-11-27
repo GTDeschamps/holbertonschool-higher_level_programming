1c605363561f9207b7f7c13c65df5680c4f9eb08
GTDeschamps <g.deschamps@hotmail.fr>
2023-11-27 18:37:29 +0100

    Update Task 7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -19,10 +19,8 @@ if __name__ == "__main__":
     session = Session(engine)
 
 # Retrieve and display all State objects sorted by id
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
+    for state in session.query(State).order_by(State.id).all()
+    print("{}: {}".format(state.id, state.name))
 
 # close session
 session.close()
