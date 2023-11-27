bfecb585ff43ae232c2899fbb6346e6a2c0fcf19
GTDeschamps <g.deschamps@hotmail.fr>
2023-11-27 19:30:53 +0100

    Task 14: Cities in State

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+import sys
+from model_state import Base, State
+from model_city import City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    # Establishing connection to Mysql server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # Create a session
+    session = Session(engine)
+
+    # Retrieve and display all State objects sorted by id
+    cities = session.query(State, City).join(City).filter(
+        State.id == City.state_id).order_by(City.id).all()
+    for state, city in cities:
+        print("{}: {} {}".format(state.name, city.id, city.name))
+
+    # close session
+    session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Create Link class to table in database"""
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+# create a base class
+Base = declarative_base()
+
+
+# define the State class
+class SCity(Base):
+    """ take the attribute of State in database"""
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'),
+                      nullable=False)
