3c38ce9bc2039372c0498e2a4494b3e8bf82287f
GTDeschamps <g.deschamps@hotmail.fr>
2023-11-27 18:51:59 +0100

    Task 8: first State

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""Start link class to table in database"""
+
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    # Establishing connection to Mysql server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # Create a session
+    session = Session(engine)
+
+    # Retrieve and display all State objects sorted by id
+    for first_state in session.query(State).order_by(State.id).all():
+        if first_state:
+            print("{}: {}".format(first_state.id, first_state.name))
+        else:
+            print("Nothing")
+
+    # close session
+    session.close()
