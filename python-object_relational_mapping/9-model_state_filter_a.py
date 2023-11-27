33833dfb7042c52b410a50e2116d57ce6ec819e8
GTDeschamps <g.deschamps@hotmail.fr>
2023-11-27 19:08:53 +0100

    Task 9: State name contain a

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Start link class to table in database"""
+
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    # Establishing connection to Mysql server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # Create a session
+    session = Session(engine)
+
+    # Retrieve and display all State objects sorted by id
+    contain_a= session.query(State).filter(State.name.like('%a%')).order_by(
+        State.id)
+    for state in contain_a:
+        print("{}: {}".format(state.id, state.name))
+
+    # close session
+    session.close()
