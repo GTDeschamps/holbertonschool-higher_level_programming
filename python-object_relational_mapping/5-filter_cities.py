cad5ffc941754f91f1d5b2e5d22cdb15f1c52e20
GTDeschamps <g.deschamps@hotmail.fr>
2023-11-27 14:25:02 +0100

    Update Task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -24,12 +24,12 @@ def list_states():
     # Execute the query
     cur.execute("SELECT cities.name FROM cities "
                 "JOIN states ON cities.state_id = states_id "
-                "WHERE states.name = %(arg)s ORDER BY cities.id ASC",
+                "WHERE states.name = %(arg)s ORDER BY cities.id",
                 {'arg': sys.argv[4]})
 
     # Fetch all the rows and display results
     query_rows = cur.fetchall()
-    cities = [row[0]for row in query_rows]
+    cities = [row[0] for row in query_rows]
     print(", ".join(cities))
 
     # Close connection
